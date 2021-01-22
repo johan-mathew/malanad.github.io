e22aa621eec7c69ad053150adf443a54f3771de1
mathewjo99 <31700508+mathewjo99@users.noreply.github.com>
2021-01-23 01:17:10 +0530

    fixes

--- main.js
+++ main.js
@@ -42,15 +42,18 @@ function openSidebar(){
 function closeSidebar(){
   document.getElementById("sidebar").style.right = "-100vw";
 }
-function viewAnswer(click_id) {
-    var x = document.getElementById("a-"+click_id);
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
 
+function viewAnswer(click_id) {
+  var x = document.getElementById("a-"+click_id);
+  var plus = document.querySelector("#"+ click_id + " .faq-plus");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    plus.innerHTML = "-";
+  } else {
+    x.style.display = "none";
+    plus.innerHTML = "+";
+  }
+}
   var swiper = new Swiper('#header-carousel', {
     spaceBetween: 0,
     pagination: {
